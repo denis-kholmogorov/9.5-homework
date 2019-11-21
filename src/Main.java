bd4c160943f269219bb1c3b0b189d824a596ace4
denis-kholmogorov <kholmogorov.denis@gmail.com>
2019-11-21 13:41:54 +0300

    version 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,33 @@
 import java.io.File;
+import java.util.Scanner;
 
 public class Main
 {
 
     public static long weigth;
+    public static int kilobyte = 1024;
+    public static int megabyte = 1024 * 1024;
+    public static int gigabyte = 1024 * 1024 * 1024;
 
     public static void main(String[] args)
     {
+        for(;;){
+            System.out.println("Введите путь файла. Пример: home/user/Рабочий стол ");
+            try {
+                Scanner scanner = new Scanner(System.in);
+                String path = scanner.nextLine();
+                File folder = new File(path);
+                isFolder(folder);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
 
-        String path = "/home/denis/IdeaProjects";
-        try {
-            File folder = new File(path);
-            isFolder(folder);
-        }catch (Exception ex) {
-            ex.printStackTrace();
+            System.out.println("Байт - " + weigth);
+            System.out.println("Килобайт - " + weigth / kilobyte);
+            System.out.println("Мегабайт - " + weigth / megabyte);
+            System.out.println("Гигабайт - " + weigth / gigabyte);
+            weigth = 0;
         }
-
-        System.out.println("Байт - " + weigth);
-        System.out.println("Килобайт - " + weigth/1024);
-        System.out.println("Мегабайт - " + weigth/1024/1024);
-        System.out.println("Гигабайт - " + weigth/1024/1024/1024);
-
     }
 
     public static void isFile(File file){
